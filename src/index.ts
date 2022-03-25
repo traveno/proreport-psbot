9b25dd44c64b3fa73171056b8d4e45b711876365
Stephen <teamgreensf@gmail.com>
2022-03-25 15:42:22 -0700

    Working on a serverside scraping solution

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('working');
+import got from 'got';
+import $ from 'cash-dom';
+import jsdom from 'jsdom';
+
+async function test() {
+    const {body} = await got('https://machinesciences.adionsystems.com/procnc/');
+    const dom = new jsdom.JSDOM(body);
+
+    $(dom.window.document).find('p').each(function() {
+        console.log($(this));
+    })
+}
+
+test();
