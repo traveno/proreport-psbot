9f3dc7053c8e4c2dab7d6033484f1d77629c0b2b
Stephen <teamgreensf@gmail.com>
2022-03-26 17:03:17 -0700

    DB file for sequelize

---
+++ src/db.ts
@@ -0,0 +1,85 @@
+import { BelongsTo, Column, DataType, ForeignKey, HasMany, Model, Sequelize, Table } from 'sequelize-typescript';
+import 'dotenv/config';
+import { Dialect } from 'sequelize/types';
+
+@Table
+export class PS_WorkOrder extends Model {
+    @Column
+    index!: string
+
+    @Column
+    status!: number
+
+    @Column
+    orderQuantity!: number
+
+    @HasMany(() => PS_RoutingRow)
+    routingRows!: PS_RoutingRow[];
+
+    @HasMany(() => PS_TrackingRow)
+    trackingRows!: PS_TrackingRow[];
+}
+
+@Table
+export class PS_RoutingRow extends Model {
+    @Column
+    op!: number;
+
+    @Column
+    opDesc!: string;
+
+    @Column
+    resource!: string;
+
+    @Column
+    completeTotal!: number;
+
+    @Column(DataType.DATE)
+    completeDate!: Date | null
+
+    @ForeignKey(() => PS_WorkOrder)
+    @Column
+    workOrderId!: number;
+
+    @BelongsTo(() => PS_WorkOrder)
+    workOrder!: PS_WorkOrder;
+}
+
+@Table
+export class PS_TrackingRow extends Model {
+    @Column
+    dateStarted!: Date;
+
+    @Column
+    dateEnded!: Date;
+
+    @Column
+    op!:  number;
+
+    @Column
+    resource!: string;
+
+    @Column
+    quantityStart!: number;
+
+    @Column
+    quantityEnd!: number;
+
+    @Column
+    quantityTotal!: number;
+
+    @ForeignKey(() => PS_WorkOrder)
+    @Column
+    workOrderId!: number;
+
+    @BelongsTo(() => PS_WorkOrder)
+    workOrder!: PS_WorkOrder;
+}
+
+export const sequelize = new Sequelize(process.env.DB_NAME!, process.env.DB_USER!, process.env.DB_PASS!, {
+    host: process.env.DB_HOST!,
+    port: Number(process.env.DB_PORT),
+    dialect: process.env.DB_DIALECT! as Dialect,
+    models: [PS_WorkOrder, PS_RoutingRow, PS_TrackingRow],
+    logging: false
+});
